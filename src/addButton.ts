e81eb38f1f36306a92a5d587107f7ee6e848d781
Masayoshi Mita <nigimitama16@gmail.com>
2026-08-16 19:08:26 +0900

    docs: コメントを追加

--- src/addButton.ts
+++ src/addButton.ts
@@ -2,7 +2,9 @@ import { addButton as addButtonOld } from "@/confit-old/addButton"
 import { addButton as addButtonNew } from "@/confit-new/addButton"
 
 if (document.URL.includes("pub.confit.atlas.jp")) {
+  // 2026年ごろからの新サイト
   addButtonNew()
 } else {
+  // 2025年ごろまでの旧サイトに向けた対応（暫定）
   addButtonOld()
 }
